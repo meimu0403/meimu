10328a5cdb0fd830c4a43a68629aa31aadc466dd
meimu <meimu@localhost>
2023-04-07 23:34:01 +0900

    課題1.19　新規作成

---
+++ HelloWorld/src/lesson19/Animal.java
@@ -0,0 +1,28 @@
+package lesson19;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Animal {
+	
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog dogA = new Dog();
+		System.out.println(dogA.name);
+		
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog dogB = new Dog(3);
+		System.out.println(dogB.dogNum);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        
+        Date d1 = calendar.getTime();
+        System.out.println(sdf.format(d1));
+        
+	}
+}
